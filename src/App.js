64c5c152be4469d32fc1b1c585b466bb9f37b349
shreshta <kakaralashreshta@gmail.com>
2025-11-07 10:33:24 +0530

    Update component imports to use components folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header';
-import Home from './Home';
-import Journeys from './Journeys';
+import Header from './components/Header';
+import Home from './components/Home';
+import Journeys from './components/Journeys';
 import './App.css';
 
 function App() {
