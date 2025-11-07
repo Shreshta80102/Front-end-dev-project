2fe475dc9d245caf8f2ac7f460f10b094e3b1dcf
shreshta <kakaralashreshta@gmail.com>
2025-11-07 10:07:24 +0530

    Replace starter App.js with Cultural Journey code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import Journeys from './Journeys';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Journeys />
+    </div>
+  );
+}
+
+export default App;
+
